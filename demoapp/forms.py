437408f08fcf5d5e7e6944e730f0efb9fa239a0a
ClaytonStudent <chen.shaohang2046@gmail.com>
2023-10-17 15:26:32 +0200

    Added Date Picker

--- demoapp/forms.py
+++ demoapp/forms.py
@@ -1,7 +1,16 @@
 from django import forms
 from .models import Product
+class DateInput(forms.DateInput):
+    input_type = "date"
 
 class ProductForm(forms.ModelForm):
 	class Meta:
 		model = Product
-		fields =  '__all__'
+		#fields =  '__all__'
+		fields = ('name','date','price','discount','note','image')
+		labels = {
+			'discount': 'Discount (%)',
+		}
+		widgets = {
+            "date": DateInput(),
+}
--- demoapp/models.py
+++ demoapp/models.py
@@ -1,11 +1,12 @@
 from django.db import models
-
+from decimal import Decimal
 # Create your models here.
 class Product(models.Model):
+    id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=255)
     price = models.DecimalField(max_digits=8,decimal_places=2)
-    discount = models.DecimalField(max_digits=4,decimal_places=2,default=1)
-    #price_with_discount = models.DecimalField(max_digits=4,decimal_places=2)
+    discount = models.DecimalField(max_digits=4,decimal_places=3,default=1)
+    price_with_discount = models.DecimalField(max_digits=8,decimal_places=2,null=True,blank=True)
     image = models.ImageField(upload_to='image/', default='image/default.jpg',null=True,blank=True)
     #slug = models.SlugField(max_length=255)
     date = models.DateField()
@@ -13,3 +14,10 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
+    def save(self, *args, **kwargs):
+        # Calculate the price with discount
+        #discount_percentage = Decimal('0.01')
+        self.price_with_discount = self.price * self.discount #* discount_percentage
+
+        # Call the original save method
+        super(Product, self).save(*args, **kwargs)
